b9d1cc595b6df3f048b3a0fbef6f68d0463a90c7
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 08:17:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Stack_Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new Stack();
+
+            stack.Push("Sam");
+            stack.Push(DateTime.Now);
+            stack.Push(5);
+
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+
+            stack.Clear();
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_Design
+{
+    public class Stack
+    {
+        private readonly List<object> stack = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                stack.Insert(0, obj);
+            }
+            else
+            {
+                Console.WriteLine("Null cannot be passed into the stack.");
+                Environment.Exit(0);
+            }
+        }
+
+        public object Pop()
+        {
+            object returnValue = null;
+
+            try
+            {
+                var removed = stack[0];
+                stack.RemoveAt(0);
+                returnValue = removed;
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Cannot remove element from an empty list.");
+            }
+
+            return returnValue;
+        }
+        public void Clear()
+        {
+            stack.Clear();
+        }
+    }
+}
